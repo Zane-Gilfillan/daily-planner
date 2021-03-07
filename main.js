fcb33e907d8aa5e9e3833462b82dce3376bf74e4
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-03-07 16:39:58 -0600

    finished ten-am section

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-//display date up to the hour using DayJs. see readme for more information
+//dayJS code. see readme for more information
 const datePlace = document.querySelector('.date-holder')
 const dayJsObject = dayjs();
 datePlace.innerHTML = dayJsObject.format("DD//MMM//YYYY HH:mm")
 
-//main save functions
+//NINE AM FUNCTIONALITY
 const nineSave = document.getElementById('nine-save')
 const nineBtn = $('#nine-btn')
 
@@ -13,11 +13,40 @@ nineBtn.click( () => {
     localStorage.setItem('nine-am-task', nineTask)
 
     renderNineTask();
-})
+    $('#nine-save').removeClass('hide')
+});
 
 function renderNineTask() {
     let nineData = localStorage.getItem('nine-am-task')
     nineSave.textContent = nineData
+    
+    //need to work on logic for hiding text areas that are blank
+    if($('#nine-save').textContent !== "") {
+         $('#nine-save').removeClass('hide')
+    } 
 }
-
 renderNineTask();
+
+//TEN AM FUNCTIONALITY
+const tenSave = document.getElementById('ten-save')
+const tenBtn = $('#ten-btn')
+
+tenBtn.click( () => {
+    let tenTask = document.getElementById('ten-input').value;
+    localStorage.setItem('ten-am-task', tenTask)
+
+    renderTenTask();
+    $('#ten-save').removeClass('hide')
+});
+
+function renderTenTask() {
+    let tenData = localStorage.getItem('ten-am-task')
+    tenSave.textContent = tenData
+
+    if($('#ten-save').value == "") {
+         $('#ten-save').addClass('hide')
+     } else {
+         $('#ten-save').removeClass('hide')
+    }
+}
+renderTenTask();
