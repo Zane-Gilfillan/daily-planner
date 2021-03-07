9bc6b68385067cfd4b6c883b9184aa31ce20a6fe
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-03-07 14:39:15 -0600

    local storage is set up an running

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ const datePlace = document.querySelector('.date-holder')
 const dayJsObject = dayjs();
 datePlace.innerHTML = dayJsObject.format("DD//MMM//YYYY HH:mm")
 
+//main save functions
+const nineSave = document.getElementById('nine-save')
+const nineBtn = $('#nine-btn')
+
+nineBtn.click( () => {
+    let nineTask = document.getElementById('nine-input').value;
+    localStorage.setItem('nine-am-task', nineTask)
+
+    renderNineTask();
+})
+
+function renderNineTask() {
+    let nineData = localStorage.getItem('nine-am-task')
+    nineSave.textContent = nineData
+}
+
+renderNineTask();
