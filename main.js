08f7904cbcd758f21adfcf0c6c3237e7fbe6ab33
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-03-08 13:09:21 -0600

    adding local storage to each hour of the planner

--- main.js
+++ main.js
@@ -11,20 +11,18 @@ const nineBtn = $('#nine-btn')
 nineBtn.click( () => {
     let nineTask = document.getElementById('nine-input').value;
     localStorage.setItem('nine-am-task', nineTask)
-
     renderNineTask();
-    $('#nine-save').removeClass('hide')
+    
 });
+
 
 function renderNineTask() {
     let nineData = localStorage.getItem('nine-am-task')
     nineSave.textContent = nineData
-    
-    //need to work on logic for hiding text areas that are blank
-    if($('#nine-save').textContent !== "") {
-         $('#nine-save').removeClass('hide')
-    } 
+    $('#nine-save').removeClass('hide')
+
 }
+
 renderNineTask();
 
 //TEN AM FUNCTIONALITY
@@ -36,17 +34,72 @@ tenBtn.click( () => {
     localStorage.setItem('ten-am-task', tenTask)
 
     renderTenTask();
-    $('#ten-save').removeClass('hide')
+    
 });
 
 function renderTenTask() {
     let tenData = localStorage.getItem('ten-am-task')
     tenSave.textContent = tenData
+    $('#ten-save').removeClass('hide')
 
-    if($('#ten-save').value == "") {
-         $('#ten-save').addClass('hide')
-     } else {
-         $('#ten-save').removeClass('hide')
-    }
 }
+
 renderTenTask();
+
+//ELEVEN AM FUNCTIONALITY
+const elevenSave = document.getElementById('eleven-save')
+const elevenBtn = $('#eleven-btn')
+
+elevenBtn.click( () => {
+    let elevenTask = document.getElementById('eleven-input').value;
+    localStorage.setItem('eleven-am-task', elevenTask)
+
+    renderElevenTask();
+})
+
+function renderElevenTask() {
+    let elevenData = localStorage.getItem('eleven-am-task')
+    elevenSave.textContent = elevenData
+    $('#eleven-save').removeClass('hide')
+
+}
+
+renderElevenTask();
+
+//TWELVE PM FUNCTIONALITY
+const twelveSave = document.getElementById('twelve-save')
+const twelveBtn = $('#twelve-btn')
+
+twelveBtn.click( () => {
+    let twelveTask = document.getElementById('twelve-input').value;
+    localStorage.setItem('twelve-pm-task', twelveTask)
+
+    renderTwelveTask()
+})
+
+function renderTwelveTask() {
+    let twelveData = localStorage.getItem('twelve-pm-task')
+    twelveSave.textContent = twelveData
+    $('#twelve-save').removeClass('hide')
+}
+
+renderTwelveTask();
+
+//ONE PM FUNCTIONALITY
+const oneSave = document.getElementById('one-save')
+const oneBtn = $('#one-btn')
+
+oneBtn.click( () => {
+    let oneTask = document.getElementById('one-input').value;
+    localStorage.setItem('one-pm-task', oneTask)
+
+    renderOneTask()
+})
+
+function renderOneTask() {
+    let oneData = localStorage.getItem('one-pm-task')
+    oneSave.textContent = oneData
+    $('#one-save').removeClass('hide')
+}
+
+renderOneTask();
